8385e4338cbd3f772822ab2474ff7958483d2427
Arvind Chauhan <psych3080@gmail.com>
2022-03-03 20:08:06 +0530

    query to fetch data

--- services/index.js
+++ services/index.js
@@ -1,1 +1,39 @@
-import { request,gql } from "graphql-request";
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+    const query = gql`
+    query MyQuery {
+        postsConnection {
+          edges {
+                node {
+                    title
+                    slug
+                    excerpt
+                    featuredImage {
+                        url
+                    }
+                    createdAt
+                    author {
+                        bio
+                        name
+                        id
+                        photo {
+                            url
+                        }
+                    }
+                    categories {
+                        name
+                        slug
+                    }
+                }
+            }
+        }
+      }
+    `
+
+    const result = await request(graphqlAPI, query)
+    console.log(result)
+    return result.postsConnection.edges;
+} 
