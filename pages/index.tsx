5d0b87f263c1e5bfd4cff11ac51670f8feaafc0e
Arvind Chauhan <psych3080@gmail.com>
2022-03-07 18:51:19 +0530

    added components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,32 @@
-import Head from 'next/head'
-import { PostCard, Categories, PostWidget } from '../components'
-import { getPosts } from '../services'
+import { FeaturedPosts } from '../sections/index';
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-// const post = [{ title: 'react test', excerpt: "learn react testing..." }, { title: 'react test with tailwind', excerpt: "learn react testing.learn react testing....." }]
-
-export default function Home({posts}) {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>CMS Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post) => (<PostCard post={post.node} key={post.title} />))}
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
-          <div className='lg:sticky relative top-8'>
-            <PostWidget />
+          <div className="lg:sticky relative top-8">
+            <PostWidget categories={undefined} slug={undefined} />
             <Categories />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
+// Fetch data at build time
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
-
   return {
-    props: { posts }
-  }
+    props: { posts },
+  };
 }
-
